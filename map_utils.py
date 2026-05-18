b7e90580b3ea6f366cfc960931f3331dd06f5d74
joshibelike <snjkrkdl@gmail.com>
2026-05-18 15:56:42 +0500

    API 3. АПТЕКИ

--- map_utils.py
+++ map_utils.py
@@ -1,10 +1,11 @@
 import requests
 
 
-def get_object_coordinates(query):
+def get_coordinates(address):
     geocode_url = "http://geocode-maps.yandex.ru/1.x/"
     params = {
-        "geocode": query,
+        "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+        "geocode": address,
         "format": "json"
     }
 
@@ -21,44 +22,16 @@ def get_object_coordinates(query):
     return None
 
 
-def get_object_size(lon, lat, query):
-    geocode_url = "http://geocode-maps.yandex.ru/1.x/"
+def get_map_params_with_pts(center, pts, spn=None):
+    lon, lat = center
+
+    if spn is None:
+        spn = [0.05, 0.05]
+
     params = {
-        "geocode": query,
-        "format": "json"
-    }
-
-    response = requests.get(geocode_url, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            envelope = data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["boundedBy"]["Envelope"]
-            lower_corner = envelope["lowerCorner"].split()
-            upper_corner = envelope["upperCorner"].split()
-
-            min_lon = float(lower_corner[0])
-            min_lat = float(lower_corner[1])
-            max_lon = float(upper_corner[0])
-            max_lat = float(upper_corner[1])
-
-            size_lon = abs(max_lon - min_lon)
-            size_lat = abs(max_lat - min_lat)
-
-            return [size_lon, size_lat]
-        except (IndexError, KeyError):
-            pass
-
-    return [0.01, 0.01]
-
-
-def get_map_params(coordinates, size):
-    lon, lat = coordinates
-    size_lon, size_lat = size
-
-    return {
         "ll": f"{lon},{lat}",
-        "spn": f"{size_lon},{size_lat}",
+        "spn": f"{spn[0]},{spn[1]}",
         "l": "map",
-        "pt": f"{lon},{lat},pm2rdm"
+        "pt": "~".join(pts)
     }
+    return params
