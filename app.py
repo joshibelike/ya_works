11705888fddc493896c280d61b4d2243d114ad17
joshibelike <snjkrkdl@gmail.com>
2026-05-19 19:53:11 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    title = "Колонизация Марса"
+    return render_template('base.html', title=title)
+
+if __name__ == '__main__':
+    app.run(debug=True)
